7f703f675c3b35086645cc3492ab39b342378168
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2023-02-27 23:25:25 -0600

    ajuste en vista

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-require('./Controller/c_verPeliculas.php');
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de peliculas</title>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css">
+    <script type="text/javascript" charset="utf8" src="https:///cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+</head>
+
+<body>
+
+    <?php
+    require('./Controller/c_verPeliculas.php');
+    ?>
+
+</body>
